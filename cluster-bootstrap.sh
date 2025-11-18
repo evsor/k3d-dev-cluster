92a0311c9bf6ca68d23014cafa24e6d0521a1262
evsor <evgenios.soroka@gmail.com>
2025-11-18 05:44:49 +0200

    Update cluster bootstrap values with templating

--- cluster-bootstrap.sh
+++ cluster-bootstrap.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-BOOTSTRAP_PATH=cluster/bootstrap
-CLUSTER_NAME=k3d-dev
-SKIP_CILIUM_TESTS=true
-GATEWAY_API_VERSION="1.2.0"
+########################################
+# K3D Dev Cluster Bootstrap Script
+########################################
 
+# Cluster Configuration Variables
+export KUBE_API_IP="0.0.0.0"
+export KUBE_API_PORT="6443"
+export CLUSTER_SUBNET=172.20.0.0/16
+export NODES_SUBNET=10.42.0.0/16
+
+# k3d fixes
 export K3D_FIX_MOUNTS=1
 export K3D_FIX_DNS=0
 
-k3d cluster create --config $BOOTSTRAP_PATH/k3d/values.yaml
+# script variables
+BOOTSTRAP_PATH=cluster/bootstrap
+CLUSTER_NAME=k3d-dev
+SKIP_CILIUM_TESTS=true
 
+# Render config files with envsubst
+cat $BOOTSTRAP_PATH/k3d/values.yaml | envsubst > /tmp/k3d-values.yaml
+cat $BOOTSTRAP_PATH/cilium/values.yaml | envsubst > /tmp/cilium-values.yaml
+
+# Create k3d cluster
+k3d cluster create --config /tmp/k3d-values.yaml
+
+# Get Master Node IP
 MASTER_NODE_IP=$(kubectl --context $CLUSTER_NAME get node/$CLUSTER_NAME-server-0 -o wide --no-headers | awk '{ print $6 }')
 echo "Master Node IP: $MASTER_NODE_IP"
 
@@ -18,7 +35,7 @@ helm template eg-crds oci://docker.io/envoyproxy/gateway-crds-helm --version v0.
 
 # Install Cilium with Helm
 helm repo add cilium https://helm.cilium.io/
-helm install cilium cilium/cilium --version 1.18.2 --set k8sServiceHost=$MASTER_NODE_IP --values $BOOTSTRAP_PATH/cilium/values.yaml --namespace kube-system
+helm install cilium cilium/cilium --version 1.18.2 --set k8sServiceHost=$MASTER_NODE_IP --values /tmp/cilium-values.yaml --namespace kube-system
 cilium status --wait
 
 if [ "$SKIP_CILIUM_TESTS" == "false" ]; then
--- cluster-cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CLUSTER_NAME=dev
-
-echo "Deleting k3d cluster"
-k3d cluster delete $CLUSTER_NAME
-
-echo "Cleanup complete."
